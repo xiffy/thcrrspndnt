bd37b792426aa1ae94755155b6714e1d583cda76
xiffy <xiffy@xiffy.nl>
2020-03-10 14:05:12 +0100

    Author URL has a slash at the end, that way we can easily link to the rss feed.

--- thcrrspndnt/thcrrspndnt.py
+++ thcrrspndnt/thcrrspndnt.py
@@ -12,28 +12,34 @@ def home():
     payload = render_template('home.html', articles=articles, start=int(start), amount=int(amount))
     return payload
 
+def rss():
+    articles = Article().get_paged(start=0, amount=100)
+    payload = render_template('rss.xml', articles=articles)
+    return Response(payload, mimetype='text/xml')
+
 def author(name):
     start, amount = pager_args()
     articles = Article().get_author_paged(name, start=start, amount=amount)
-    payload = render_template('author.html', articles=articles, static_depth='..', author=name,
+    payload = render_template('author.html', articles=articles, static_depth='../..', author=name,
                               start=int(start), amount=int(amount))
     return payload
 
-def rss():
-    articles = Article().get_paged()
+def rss_author(name):
+    articles = Article().get_author_paged(name, start=0, amount=100)
     payload = render_template('rss.xml', articles=articles)
     return Response(payload, mimetype='text/xml')
 
 def pager_args():
     start = request.args.get('start', 0)
-    amount = request.args.get('amount', 10)
+    amount = request.args.get('amount', 20)
     return (start, amount)
 
 def create_thcrrspndnt():
     app = Flask('thcrrspndnt')
     app.add_url_rule('/', view_func=home)
-    app.add_url_rule('/author/<name>', view_func=author)
     app.add_url_rule('/rss', view_func=rss)
+    app.add_url_rule('/author/<name>/', view_func=author)
+    app.add_url_rule('/author/<name>/rss', view_func=rss_author)
     return app
 
 app = create_thcrrspndnt()
