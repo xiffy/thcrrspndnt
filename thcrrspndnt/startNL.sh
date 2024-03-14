cdab8dab2467a59c4c4860d3dd83c63bf71af8bf
Appie Verschoor <xiffy@molecule.nl>
2024-03-14 19:03:56 +0100

    Afblijven!

--- thcrrspndnt/startNL.sh
+++ thcrrspndnt/startNL.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FLASK_DEBUG=1
 export CORRES_VERSION=NL
-#cd /home/xiffy/project/thcrrspndnt/thcrrspndnt
-../venv/Scripts/flask.exe --app=thcrrspndnt run --host=0.0.0.0 --port=5002 --debug
+cd /home/xiffy/project/thcrrspndnt/thcrrspndnt
+export FLASK_APP=/home/xiffy/project/thcrrspndnt/thcrrspndnt/thcrrspndnt.py ; flask run --host=0.0.0.0 --port=5002
