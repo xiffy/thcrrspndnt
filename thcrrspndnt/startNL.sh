4356f65274612c28d9401e1393e445692038403f
xiffy <xiffy@xiffy.nl>
2024-04-06 16:45:40 +0200

    find out yourself

--- thcrrspndnt/startNL.sh
+++ thcrrspndnt/startNL.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export FLASK_DEBUG=1
 export CORRES_VERSION=NL
-cd /home/xiffy/project/thcrrspndnt/thcrrspndnt
-export FLASK_APP=/home/xiffy/project/thcrrspndnt/thcrrspndnt/thcrrspndnt.py ; flask run --host=0.0.0.0 --port=5002
+export FLASK_APP=/home/xiffy/develop/thcrrspndnt/thcrrspndnt/thcrrspndnt.py ; flask run --host=0.0.0.0 --port=5002
