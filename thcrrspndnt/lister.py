1e756d97b91aba21a0bb78ba7ca5d1f63d8e82ea
Appie Verschoor <xiffy@xiffy.nl>
2025-03-14 14:23:23 +0100

    just a list since selenium stopped working

--- thcrrspndnt/lister.py
+++ thcrrspndnt/lister.py
@@ -1,5 +1,7 @@
 import settings
 from model.db import Db
+from model.unshorten import Unshorten
+
 from model.article import Article
 
 
@@ -9,5 +11,10 @@ with open(settings.CONFIG["urllist"]) as urllist:
     urls = [line.rstrip() for line in urllist]
 
 for url in urls:
+    if site not in url:
+        url = Unshorten(url)
     if site in url:
+        print(url)
         article = Article.maybe_find_or_create(url)
+    else:
+        print("Noooooooooooooooooooooooooooooooo!", url)
