bc3a2779a972505ddf7cc5cfdc0b9646dd1fa009
Appie Verschoor <xiffy@xiffy.nl>
2025-03-14 14:28:57 +0100

    unshorten my ass

--- thcrrspndnt/lister.py
+++ thcrrspndnt/lister.py
@@ -4,7 +4,7 @@ from model.unshorten import Unshorten
 
 from model.article import Article
 
-
+db = Db()
 site = settings.CONFIG.get("site", "decorrespondent.nl")
 urls = []
 with open(settings.CONFIG["urllist"]) as urllist:
@@ -12,7 +12,7 @@ with open(settings.CONFIG["urllist"]) as urllist:
 
 for url in urls:
     if site not in url:
-        url = Unshorten(url)
+        url = Unshorten(db=db, url=url).as_class()
     if site in url:
         print(url)
         article = Article.maybe_find_or_create(url)
